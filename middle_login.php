6d96c1869d2885082bff89303dac43bea6476f70
Alan Finnin <37178050+alanfinnin@users.noreply.github.com>
2019-05-15 19:59:11 +0100

    Colour and now you are passed straight to the chat
    Colours are now taken into account and now instead of a middle section of the login process you are taken straight to the chat page

--- middle_login.php
+++ middle_login.php
@@ -20,6 +20,7 @@
 			}
 			$counter++;
 	}
+	$colour = $colours[$counter];
 	if($check == 0){
 		session_unset(); 
 		ob_start();
@@ -29,30 +30,6 @@
 	}
 	$_SESSION['status'] = "Active";
 	$_SESSION['username'] = $username;
+	$_SESSION['colour'] = $colour;
+	header("Location: http://www.skynet.ie/~alanfinnin/login/login.php");
 ?>
-<html>
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
-	
-	<link href="addons/font-awesome/css/fontawesome-5.2.0/css/all.min.css" rel="stylesheet" type="text/css">
-	<link href="css/main.css" rel="stylesheet" type="text/css">
-	<link href="css/@media.css" rel="stylesheet" type="text/css">
-	<link rel="icon" 
-      type="image/jpg" 
-      href="images/icon.jpg" />
-	<head>
-	
-	<head>
-		<title>Login</title>
-	</head>
-	<body>
-		<h1 class = "main_text">Welcome <?php echo($username) ?></h1>
-		<h3 class = "secondary_text">You have successfully logged in<br>Click here to carry on to the home page</h3>
-		<form class = "secondary_text" action="login.php" method="post">
-				<input class = "submit_button" type="submit" value="Carry on">
-		</form>
-		
-	</body>
-</html>
