52ea73c0eb81f7e56a1ba253659c158bbf4025fb
Alan Finnin <37178050+alanfinnin@users.noreply.github.com>
2019-05-15 20:01:09 +0100

    Colour modification
    Taking the red blue swap out and replacing with the new colour per user method

--- passive_pull.php
+++ passive_pull.php
@@ -6,13 +6,7 @@
 			$file_handle = fopen("info/chat.txt", "r");
 			while (!feof($file_handle)) {
 				$line = fgets($file_handle);
-				if($counter % 2 == 0){
-					echo '<div class = "my_red"> ' . $line . "</div>";
-				}
-				else{
-					echo '<div class = "my_blue"> ' . $line . "</div>";
-				}
-				echo "<br>";
+				echo $line;
 				$counter++;
 			}
 		fclose($file_handle);
@@ -23,8 +17,4 @@
 	function insertAtPosition($string, $insert, $position) {
 		return implode($insert, str_split($string, $position));
 	}
-	/*
-		Worm thingy:	
-		<iframe src="http://www.staggeringbeauty.com/" style="border: 1px inset #ddd" width="498" height="598"></iframe>
-	*/
 ?>
